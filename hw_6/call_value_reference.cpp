889c467f18e3d2232eb550cf136a06096b899051
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-04-23 17:30:11 -0700

    update

--- hw_6/call_value_reference.cpp
+++ hw_6/call_value_reference.cpp
@@ -7,18 +7,20 @@
 #include <cstdlib>
 #include <cstdio>
 #include <locale>
+#include <string>
 
+#define CSV_IO_NO_THREAD
 
 using namespace std;
 
-char change_name_reference(char& name, multimap<char, char> states_map){
-    char new_name = states_map.find(name)->second;
+string change_name_reference(string& name, multimap<string, string> states_map){
+    string new_name = states_map.find(name)->second;
     name = new_name;
     return name;
 }
 
-char change_name_value(char name, multimap<char, char> states_map){
-    char new_name = states_map.find(name)->second;
+string change_name_value(string name, multimap<string, string> states_map){
+    string new_name = states_map.find(name)->second;
     name = new_name;
     return name;
 }
@@ -26,51 +28,59 @@ char change_name_value(char name, multimap<char, char> states_map){
 
 int main() {
 
-    /*
     io::CSVReader<2> in1("states.csv");
-    in1.read_header(io::ignore_extra_column, "State", "Abbreviation");
-    char state_name; char state_abbr;
-    multimap <char, char> states_map;
+    in1.read_header(io::ignore_extra_column, "state", "abbreviation");
+    string state_name; string state_abbr;
+    multimap <string, string> states_map;
     int i=0;
     while(in1.read_row(state_name, state_abbr)){
-        states_map.insert(pair <char, char> (state_abbr, state_name));
+        states_map.insert(pair <string, string> (state_abbr, state_name));
         ++i;
     }
-    */
 
-
-    io::CSVReader<1> in2("Parking_Violations_Issued_-_Fiscal_Year_2018.csv");
-    in2.read_header(io::ignore_extra_column, "Registration State");
-    char registration_state;
-    char registration_state_list[8643263];
-    int j=0;
-    while(in2.read_row(registration_state)){
-        cout << j <<endl;
-        registration_state_list[j] = registration_state;
-        ++j;
-    }
-
-    /*
     clock_t tStart = clock();
+
 
     printf("Start of reference call");
     tStart = clock();
-    int k=0;
-    for (auto name : registration_state_list){
-        registration_state_list[i] = change_name_reference(name, states_map);
-        ++k;
+
+    io::CSVReader<1> in2("Parking_Violations_Issued_-_Fiscal_Year_2018.csv");
+    in2.read_header(io::ignore_extra_column, "Registration State");
+    string registration_state1;
+    string name1;
+    int j=0;
+    while(j < 8643265){
+        try {
+            bool row_readable = in2.read_row(registration_state1);
+            name1 = change_name_reference(registration_state1, states_map);
+            j++;
+        } catch(io::error::too_many_columns e1) {
+            j++;
+        }
     }
+
     printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
 
     printf("Start of value call");
 
     tStart = clock();
-    int l=0;
-    for (auto name : registration_state_list){
-        registration_state_list[i] = change_name_value(name, states_map);
-        ++l;
+
+    io::CSVReader<1> in3("Parking_Violations_Issued_-_Fiscal_Year_2018.csv");
+    in3.read_header(io::ignore_extra_column, "Registration State");
+    string registration_state2;
+    string name2;
+    int k=0;
+    while(k < 8643265){
+        try {
+            bool row_readable = in3.read_row(registration_state2);
+            name2 = change_name_value(registration_state2, states_map);
+            k++;
+        } catch(io::error::too_many_columns e1) {
+            k++;
+        }
     }
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);*/
+    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
     return 0;
 }
